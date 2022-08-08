09b6f6ff88e84cc7ac8e1ebbb102a06af2885634
engooh <erickngooh@gmail.com>
2022-08-08 15:14:35 +0200

    push correctif parsing

--- source/share/libft/ft_strjoin.c
+++ source/share/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: engooh <erickngooh.1@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 11:41:22 by engooh            #+#    #+#             */
-/*   Updated: 2022/08/04 10:04:21 by engooh           ###   ########.fr       */
+/*   Updated: 2022/08/08 11:58:53 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -39,25 +39,3 @@ char	*ft_strjoin(char *s1, char const *s2)
 	free(tmp);
 	return (c);
 }
-/*
-int	*ft_strjoin2(int *s1, int *s2)
-{
-	int	*c;
-	int	*p;
-	int	*tmp;
-
-	if (!s1 || !s2)
-		return (NULL);
-	p = malloc((ft_strlen2(s1) + ft_strlen2(s2) + 1) * sizeof(int));
-	if (!p)
-		return (NULL);
-	c = p;
-	tmp = s1;
-	while (*s1)
-		*p++ = *s1++;
-	while (*s2)
-		*p++ = *s2++;
-	*p = '\0';
-	free(tmp);
-	return (c);
-}*/
---
+++ test.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: engooh <engooh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 12:38:35 by engooh            #+#    #+#             */
+/*   Updated: 2022/08/08 12:46:49 by engooh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ft_function(void)
+{
+	int nbr;
+
+	nbr = 4;
+}
+
+int main(void)
+{
+	char	*content;
+	int	nbr;
+	int	nbr1;
+
+	nbr = 3;
+	nbr1 = 5;
+	printf("test %d, %d\n", nbr, nbr + (8 * 4));
+	return (0);
+}
